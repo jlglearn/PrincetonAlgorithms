f07f46c7cc77255174e951195744dbff0b28bb39
Jose Luis Gonzalez <jlglearn@gmail.com>
2013-09-25 10:58:27 -0500

    Add Fast.java

---
+++ Fast.java
@@ -0,0 +1,129 @@
+import java.util.Arrays;
+
+public class Fast
+{
+    public Fast()
+    {
+    }
+    
+    public static void main(String argv[])
+    {     
+        ReadPoints();
+        PrepareDraw();
+        DrawPoints();
+        FindCollinearPoints();
+    }
+    
+    private static void PrepareDraw()
+    {
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+    }
+    
+    
+    private static void ReadPoints()
+    {
+        int nPoints = StdIn.readInt();
+        points = new Point[nPoints];
+        
+        for (int i = 0; i < nPoints; i++)
+        {
+            int x = StdIn.readInt();
+            int y = StdIn.readInt();
+            
+            points[i] = new Point(x, y);
+        }            
+    }
+    
+    private static void DrawPoints()
+    {
+        for (int i = 0; i < points.length; i++)
+        {
+            points[i].draw();
+        }
+    }
+    
+    private static void FindCollinearPoints()
+    {        
+        // first create a working copy of the points
+        Point[] p = new Point[points.length];
+        for (int i = 0; i < points.length; i++)
+            p[i] = points[i];
+        
+        for (int p0 = 0; p0 < points.length; p0++)
+        {                    
+            Arrays.sort(p, points[p0].SLOPE_ORDER);
+            
+            int i = 1;
+
+            while ((i < p.length) && (p[0].compareTo(p[i]) == 0)) i++;
+                        
+            // i is either past the end of the array or
+            // in the typical case points to the first
+            // point not equal to the reference point
+            
+            if (i >= p.length) continue;
+            
+            double lastSlope = -1 * Double.POSITIVE_INFINITY;
+            int streak = 1;
+            
+            for (int startCollinear = i; i < p.length; i++)
+            {
+                double slope = p[0].slopeTo(p[i]);
+                
+                if (slope == lastSlope)
+                    streak++;
+                
+                if ((streak >= 3) &&
+                    ((i == p.length-1) || (slope != lastSlope)))
+                {
+                    Point[] cp = new Point[streak+1];
+                    
+                    // the first collinear point is the reference point
+                    cp[0] = p[0];
+                    
+                    // copy the rest of the collinear points
+                    for (int j = 1, k = startCollinear; j < streak+1; j++, k++)
+                        cp[j] = p[k];
+                        
+                    Arrays.sort(cp);
+                    PrintCollinearPoints(cp);
+                }
+                
+                if (slope != lastSlope)
+                {
+                    // reference point is first collinear point, i is second collinear point
+                    streak = 1;
+                    startCollinear = i;
+                }
+                
+                lastSlope = slope;
+            } 
+            
+        }
+        
+    }
+    
+    private static void PrintCollinearPoints(Point[] p)
+    {
+        for (int i = 0; i < p.length; i++)
+            StdOut.print(((i > 0) ? " -> " : "") + p[i]);
+            
+        StdOut.println();
+        
+        for (int i = 0; i < p.length - 1; i++)
+            p[i].drawTo(p[i+1]);
+    }
+            
+    private static void SortPoints(Point p)
+    {
+        Arrays.sort(points, p.SLOPE_ORDER);
+    }
+    
+    private static void SortPoints()
+    {
+        Arrays.sort(points);
+    }
+    
+    private static Point points[];
+}
